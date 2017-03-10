44820a8d4e3e597ec526e5773d33749d75dbd4be
Ryan Tercias <terciasr@us.ibm.com>
2017-03-10 01:19:35 -0500

    adds datasource badge

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,6 +11,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 })
 export class HomeComponent {
   public cities: City[];
+  public source: string;
+  private timeout = null;
+
   constructor(private weatherService: WeatherService) {}
 
   ngOnInit() {
@@ -20,9 +23,17 @@ export class HomeComponent {
       5128638   //New York 
     ];
 
-    this.weatherService.load(ids).then((cities) => {
+    this.weatherService.load(ids).then((cities) => {      
       this.cities = cities;
+      clearTimeout(this.timeout);
     });
+
+    this.source = this.weatherService.getCacheFlag();
+
+    this.timeout = setTimeout(() => {
+      this.source = this.weatherService.getCacheFlag();
+      clearTimeout(this.timeout);
+    }, 30000);    
   }
 
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -6,34 +6,43 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/toPromise';
 import City from './../entities/city';
 
+const msgAPI = 'Data from API';
+const msgCache = 'Data from cache';
+
 @Injectable()
 export default class WeatherService {
   private appId = 'cab4e299abff5d6eeadd8dd9492408cf';
   private units = 'imperial';
   private corsPrefix = 'https://cors-anywhere.herokuapp.com/';
+  private interval = 30000;
 
   constructor (private http: Http) {}
 
-  public load(cityIds: number[]) {
-    const oneHour = 30*1000;
+  public load(cityIds: number[]) {    
     const cityIdString =cityIds.join(',');
     const serviceUrl = 'http://api.openweathermap.org/data/2.5/group';
     const url = `${serviceUrl}?id=${cityIdString}&units=${this.units}&APPID=${this.appId}`;
     
-    if (this.isCacheExpired()) {
-      console.log('Getting data from API');
-      return this.http.get(url).toPromise().then((response) => {
-        let data = response.json();
-        this.setCache('weather', JSON.stringify(data));
-        this.setCache('weather-timeout', Date.now() + oneHour);        
-        return data.list.map((item) => new City(item));
-      });
-    } else {
-      console.log('Getting data from cache');
+    if (this.isCacheExpired()) {     
+       
+      return this.http.get(url)
+        .toPromise()
+        .then((response) => {
+          let data = response.json();
+
+          this.setCache('weather', JSON.stringify(data));
+          this.setCache('weather-timeout', Date.now() + this.interval);        
+
+          return data.list.map((item) => new City(item));
+        });
+
+    } else {      
+
       return new Promise((resolve, reject) => {
         try {
           let data = this.getCache('weather');
           resolve(data.list.map((item) => new City(item)));
+
         } catch(e) {           
           this.expireCache('weather');
           this.expireCache('weather-timeout');
@@ -41,6 +50,10 @@ export default class WeatherService {
         }
       });
     }
+  }
+
+  public getCacheFlag() {    
+    return this.isCacheExpired() ? msgAPI : msgCache;
   }
 
   private setCache(key, data) {
